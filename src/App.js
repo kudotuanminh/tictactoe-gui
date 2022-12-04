3f22771b90859a87b0b529593db49be1399ff07f
Tuan Minh Ngo <catboom01@gmail.com>
2022-12-05 01:30:57 +0700

    fuck

--- src/App.js
+++ src/App.js
@@ -2,71 +2,80 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faO } from "@fortawesome/free-solid-svg-icons";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 export default function App() {
     let obj = {
-        column: 6,
-        row: 4,
-        grid: [
-            [-1, 0, 0, 1, 1, 2],
-            [0, 1, 0, 2, 1, 2],
-            [0, 0, 2, 1, 1, 0],
-            [0, 0, 0, -1, 0, 0],
-        ],
-        winner: "Player 1",
-        winMoves: [(0, 4), (1, 4), (2, 4)],
+        board: [],
     };
 
     const [data, setData] = useState(obj);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
-    const [winner, setWinner] = useState(null);
+    const url = "ws://s.vominhduc.me:9000";
+    const ws = new WebSocket(url);
 
-    // const url = "https://randomuser.me/api";
+    const matchesID = [];
+    ws.onopen = () => {
+        ws.send(
+            JSON.stringify({
+                action: 8,
+            })
+        );
+        ws.onmessage = (e) => {
+            let response = JSON.parse(e.data);
+            response.matches.forEach((match) => {
+                let obj = {
+                    label: String(match.matchId),
+                    id: match.matchId,
+                };
+                matchesID.push(obj);
+            });
+        };
+    };
+    const [match, setMatch] = useState(19);
+
+    const initWebsocket = () => {
+        ws.onopen = () => {
+            ws.send(
+                JSON.stringify({
+                    action: 9,
+                    match: match,
+                })
+            );
+            ws.onmessage = (event) => {
+                const response = JSON.parse(event.data);
+                setData(response);
+                setLoading(false);
+
+                console.log(response);
+            };
+        };
+    };
+    useEffect(() => {
+        initWebsocket();
+    }, [match]);
 
     const [rows, setRows] = useState([]);
     const [row, setRow] = useState([]);
 
-    // useEffect(() => {
-    //     fetch(url)
-    //         .then((response) => {
-    //             if (response.ok) {
-    //                 return response.json();
-    //             }
-    //             throw response;
-    //         })
-    //         .then((data) => {
-    //             setData(data);
-    //         })
-    //         .catch((error) => {
-    //             console.error("Error fetching data: ", error);
-    //             setError(error);
-    //         })
-    //         .finally(() => {
-    //             setLoading(false);
-    //         });
-    // });
-
     useEffect(() => {
-        if (data.winner) {
-            setWinner(data.winner);
-        }
-
         let rows = [];
-        for (let i = 0; i < data.row; i++) {
+
+        for (let i = 0; i < data.board.length; i++) {
             let row = [];
             let className = "";
-            let value = "";
-            for (let j = 0; j < data.column; j++) {
-                switch (data.grid[i][j]) {
+            let value = null;
+            for (let j = 0; j < data.board[i].length; j++) {
+                switch (data.board[i][j]) {
                     case -1:
                         className = "box blocked";
-                        value = "";
+                        value = null;
                         break;
                     case 0:
                         className = "box empty";
-                        value = "";
+                        value = null;
                         break;
                     case 1:
                         className = "box player-1";
@@ -79,15 +88,11 @@ export default function App() {
                     default:
                         break;
                 }
-                //  TODO: check winMoves
-                if (null) {
-                    className += " win";
-                }
 
                 row.push(
                     <td className={className}>
                         <div className="value-container">
-                            <FontAwesomeIcon icon={value} />
+                            {value ? <FontAwesomeIcon icon={value} /> : null}
                         </div>
                     </td>
                 );
@@ -99,17 +104,28 @@ export default function App() {
         setRows(rows);
 
         setLoading(false);
-    }, []);
+    }, [data]);
 
     if (loading) return "Loading...";
-    if (error) return "Error!";
 
     return (
-        <div className="main-container">
-            <table className="board">
-                <tbody>{rows}</tbody>
-            </table>
-            <h1 className="announcement">{winner} wins!</h1>
+        <div>
+            <div className="main-container">
+                <Autocomplete
+                    options={matchesID}
+                    onChange={(event, newMatch) => {
+                        setMatch(newMatch.id);
+                    }}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Match" />
+                    )}
+                />
+
+                <table className="board">
+                    <tbody>{rows}</tbody>
+                </table>
+            </div>
         </div>
     );
 }
